efb840ab665507176432e8e7061235371dc7addb
pupp3tbr4 <34111982+pupp3tbr4@users.noreply.github.com>
2025-06-21 00:54:41 +0200

    Add files via upload

---
+++ contador hacia atras.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <windows.h>
+
+
+
+
+int main ()
+
+
+
+
+{
+
+int contador; //Declaramos la variable contador
+
+printf("*-Bienvenido a sistema de cuenta hacia atras-*\n");
+printf("Escribe el numero desde el que quieres contar:\n");
+scanf("%i",&contador);
+
+
+    while (contador > 0) //Creamos el bucle.
+
+    {
+        printf("Vamos por el numero %i\n",contador);
+        Beep(750, 300);  // frecuencia en Hz, duración en milisegundos
+        Sleep(1000);
+        contador--;
+
+
+     while (contador == 0)
+
+
+
+        {
+            MessageBox(NULL,"Ha llegado al final de su cuenta atras","Alert",MB_OK | MB_ICONINFORMATION);
+
+        }
+
+
+
+
+
+    }
+ return 0;
+}
